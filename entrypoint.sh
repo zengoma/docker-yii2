ca32ce208b14240b722d7e855afcb64502582f95
Lance Moore <user@example.com>
2017-09-19 15:53:27 +0200

    fixed incorrect conditional for composer installation

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 set -e
 
+cp -f /nginx.conf /nginx/conf.d/default.conf;
+
 if [ "$(ls -A vendor)" ]; then
-     yes | composer install && yes | php yii migrate up;
-     cp -f /nginx.conf /nginx/conf.d/default.conf;
-else
     yes | composer update && yes | php yii migrate up;
+else
+    yes | composer install && yes | php yii migrate up;
 fi
 
 exec "$@"
